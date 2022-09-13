db37dace1384e59966efa14748496ec225f95c53
James Lee <j.lee1203@hotmail.com>
2022-09-12 20:40:52 -0700

    commit tree bffa14655f22b021ba7ae270063f5c93afb0fdb8

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+const studentInfo = {
+    name: "Steve",
+    email: "steve@steve.com"
+};
+
+const customizeObject = {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(studentInfo),
+};
+
+function submitData(name,email){
+    return fetch("http://localhost:3000/users", customizeObject)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(customizeObject){
+        document.body.innerHTML = customizeObject.id
+    })
+    .catch(function(error){
+        alert("Unauthorized Access");
+        document.body.innerHTML = error;
+    })
+    }
